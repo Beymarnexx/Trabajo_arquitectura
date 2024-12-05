7824017bfd577e4067f9cb8cb287f7bf2fdefb7c
Beymarnexx <160280516+Beymarnexx@users.noreply.github.com>
2024-12-05 00:33:58 -0700

    Commit inicial del proyecto

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <!-- Navbar Global -->
+    <NavbarComponent />
+    <!-- Contenido de las Páginas -->
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavbarComponent from '@/components/NavbarComponent.vue'; // Importa el NavbarComponent
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    NavbarComponent, // Declara el componente
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,34 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+//import axios from "axios";
+import "bootstrap/dist/css/bootstrap.css";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+import { createRouter, createWebHistory } from 'vue-router'; // Importar funciones de Vue Router
+
+import HomePage from "./modules/pages/views/HomePage.vue";
+import ForosPage from "./modules/pages/views/ForosPage.vue";
+import PasantiasPage from "./modules/pages/views/PasantiasPage.vue";
+import GraduadosPage from "./modules/pages/views/GraduadosPage.vue";
+import LoginPage from "./modules/authentication/views/LoginPage.vue";
+import RegistroPage from "./modules/authentication/views/RegistroPage.vue";
+
+// Configuración de rutas
+const router = createRouter({
+    history: createWebHistory(), // Historial basado en navegador
+    routes: [
+        { path: "/", component: HomePage },
+        { path: "/login", component: LoginPage },
+        { path: "/registro", component: RegistroPage },
+        { path: "/foros", component: ForosPage },
+        { path: "/pasantias", component: PasantiasPage },
+        { path: "/graduados", component: GraduadosPage },
+    ],
+});
+
+// Crear instancia de la app y agregar el router
+createApp(App)
+    .use(router) // Asegúrate de usar el router
+    .mount('#app');
